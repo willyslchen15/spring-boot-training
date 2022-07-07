4577a19a64a468d0cb7af1bc6f38c670370e80c4
willyslchen15 <willyslchen@gmail.com>
2022-07-07 23:55:33 +0800

    回傳 Json 格式 - @RestControler、@Controller + @ResponseBody

--- src/main/java/com/willyslchen15/springboottraining/MyController.java
+++ src/main/java/com/willyslchen15/springboottraining/MyController.java
@@ -3,17 +3,27 @@ package com.willyslchen15.springboottraining;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/detail")
 public class MyController {
 
     @RequestMapping("/product")
-    public String product() {
-        return "Apple, Orange";
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Orange");
+        store.setProductList(list);
+        return store;
     }
 
     @RequestMapping("/user")
-    public String user() {
-        return "Name : Willy";
+    public Student user() {
+        Student student = new Student();
+        student.setName("Willy");
+        return student;
     }
 }
---
+++ src/main/java/com/willyslchen15/springboottraining/Student.java
@@ -0,0 +1,22 @@
+package com.willyslchen15.springboottraining;
+
+public class Student {
+    String name;
+    Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
