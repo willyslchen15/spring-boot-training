9ba741ac6d6f5a7f7927dd7ce0c68ba3b6cabe99
willyslchen15 <willyslchen@gmail.com>
2023-09-13 16:17:14 +0800

    Initial commit

---
+++ src/main/java/com/willyslchen15/springboottraining/SpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.willyslchen15.springboottraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTrainingApplication.class, args);
+    }
+
+}
