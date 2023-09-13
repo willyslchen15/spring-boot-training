4db5f6bd910997be9ab97d99f0ce91f12f1561c1
willyslchen15 <willyslchen@gmail.com>
2023-09-13 16:58:16 +0800

    C02_Bean 的創建 - @Component、@Configuration + @Bean

---
+++ src/main/java/com/willyslchen15/springboottraining/MyConfiguration.java
@@ -0,0 +1,17 @@
+package com.willyslchen15.springboottraining;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+    @Bean
+    public Printer myPrinter() {
+        return new HpPrinter();
+    }
+
+    @Bean
+    public Printer yourPrinter() {
+        return new CanonPrinter();
+    }
+}
