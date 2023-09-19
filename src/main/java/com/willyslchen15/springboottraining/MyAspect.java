4182ae329d67073a3b0ff250c114bb50f55833f3
willyslchen15 <willyslchen@gmail.com>
2023-09-19 15:16:15 +0800

    C03_Spring AOP 的用法 - @Around

--- src/main/java/com/willyslchen15/springboottraining/MyAspect.java
+++ src/main/java/com/willyslchen15/springboottraining/MyAspect.java
@@ -1,9 +1,13 @@
 package com.willyslchen15.springboottraining;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @Aspect
@@ -16,5 +20,18 @@ public class MyAspect {
     @After("execution(* com.willyslchen15.springboottraining.HpPrinter.*(..))")
     public void after() {
         System.out.println("I'm after.");
+    }
+
+    @Around("execution(* com.willyslchen15.springboottraining.CanonPrinter.print(..))")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Date start = new Date();
+
+        // 執行切入點的方法
+        Object obj = proceedingJoinPoint.proceed();
+
+        Date end = new Date();
+        long time = end.getTime() - start.getTime();
+        System.out.println("Time : " + time + " ms");
+        return obj;
     }
 }
